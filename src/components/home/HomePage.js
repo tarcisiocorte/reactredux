208370859ba12ad4aa104ed40302167de77ffe31
tarcisiocorte <tarcisiocorte@gmail.com>
2018-05-04 12:23:27 -0300

    fixing labels

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,7 +5,7 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1>Pluralsight Administration</h1>
+        <h1>Sys Administration</h1>
         <p>
           React, Redux and React Router, in ES6 for ultra-responsive web apps.
         </p>
