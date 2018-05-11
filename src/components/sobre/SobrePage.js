be8e198479a095e76c277ad6b222e76349874aa3
tarcisiocorte <tarcisiocorte@gmail.com>
2018-05-11 00:17:13 -0300

    replace master branch

---
+++ src/components/sobre/SobrePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+class SobrePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p> This application uses the following technologies:</p>
+      </div>
+    );
+  }
+}
+export default SobrePage;
